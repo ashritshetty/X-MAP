2043c6d6ee567ff32428a4c13b61c5ca9a7ba9ec
ashrit <ashrit09@gmail.com>
2017-09-19 20:47:14 -0400

    Moving code from Bitbucket to Github

---
+++ debug/moc_mainwindow.cpp
@@ -0,0 +1,143 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.7.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../qt-pacube/mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.7.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[10];
+    char stringdata0[113];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 12), // "open_clicked"
+QT_MOC_LITERAL(2, 24, 0), // ""
+QT_MOC_LITERAL(3, 25, 4), // "path"
+QT_MOC_LITERAL(4, 30, 12), // "save_clicked"
+QT_MOC_LITERAL(5, 43, 11), // "run_clicked"
+QT_MOC_LITERAL(6, 55, 15), // "profile_clicked"
+QT_MOC_LITERAL(7, 71, 19), // "slotCombo1Activated"
+QT_MOC_LITERAL(8, 91, 1), // "s"
+QT_MOC_LITERAL(9, 93, 19) // "slotCombo4Activated"
+
+    },
+    "MainWindow\0open_clicked\0\0path\0"
+    "save_clicked\0run_clicked\0profile_clicked\0"
+    "slotCombo1Activated\0s\0slotCombo4Activated"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       9,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    1,   59,    2, 0x0a /* Public */,
+       1,    0,   62,    2, 0x2a /* Public | MethodCloned */,
+       4,    1,   63,    2, 0x0a /* Public */,
+       4,    0,   66,    2, 0x2a /* Public | MethodCloned */,
+       5,    0,   67,    2, 0x0a /* Public */,
+       6,    1,   68,    2, 0x0a /* Public */,
+       6,    0,   71,    2, 0x2a /* Public | MethodCloned */,
+       7,    1,   72,    2, 0x0a /* Public */,
+       9,    1,   75,    2, 0x0a /* Public */,
+
+ // slots: parameters
+    QMetaType::Void, QMetaType::QString,    3,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,    3,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,    3,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,    8,
+    QMetaType::Void, QMetaType::QString,    8,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        MainWindow *_t = static_cast<MainWindow *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->open_clicked((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 1: _t->open_clicked(); break;
+        case 2: _t->save_clicked((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 3: _t->save_clicked(); break;
+        case 4: _t->run_clicked(); break;
+        case 5: _t->profile_clicked((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 6: _t->profile_clicked(); break;
+        case 7: _t->slotCombo1Activated((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 8: _t->slotCombo4Activated((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        default: ;
+        }
+    }
+}
+
+const QMetaObject MainWindow::staticMetaObject = {
+    { &QMainWindow::staticMetaObject, qt_meta_stringdata_MainWindow.data,
+      qt_meta_data_MainWindow,  qt_static_metacall, Q_NULLPTR, Q_NULLPTR}
+};
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return Q_NULLPTR;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(const_cast< MainWindow*>(this));
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 9)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 9;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 9)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 9;
+    }
+    return _id;
+}
+QT_END_MOC_NAMESPACE
